46f15c5f0928bb1a21c6c31546ec9aa837ac445b
Awoyomi tobi <tbawoyomi@gmail.com>
2021-02-21 19:52:05 +0100

    little change again

--- airline/settings.py
+++ airline/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = '+#=m0_si2dcxo#1!&=#=9n9-#nw#*3ffbn^^6(777-o-2$he6('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['awoyomiwebsite.herokuapp.com','localhost','127.0.0.1']
-
+#ALLOWED_HOSTS = ['awoyomiweb.herokuapp.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['awoyomiweb.herokuapp.com']
 
 # Application definition
 
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,22 +125,25 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 #PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
-#STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #activating django-heroku
 #django_heroku.settings(locals())
 
 
 #Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE ='whitenoise.django.CompressedManifestStaticFilesStorage'
 #STATICFILES_STORAGE ='whitenoise.django.GzipManifestStaticFilesStorage'
+
+
     
 
