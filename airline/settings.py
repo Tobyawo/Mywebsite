27f4f80834a2aa589b22471596a96dbd568caa08
Awoyomi tobi <tbawoyomi@gmail.com>
2021-02-21 20:46:55 +0100

    solving the database migration issues

--- airline/settings.py
+++ airline/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #activating django-heroku
